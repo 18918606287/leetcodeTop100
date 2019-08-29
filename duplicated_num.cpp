597696cd9e21e75bcaf5e5c7d19426ad039beeab
Erebos <erebos@rams.colostate.edu>
2019-08-30 02:36:50 +0800

    add leetcode#287

---
+++ duplicated_num.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        vector<int> count(nums.size(), 0);
+        for(int i = 0; i < nums.size(); i++){
+            if(count[nums[i]] == 1) return nums[i];
+            count[nums[i]]++;
+        }
+        return -1;
+    }
+};
